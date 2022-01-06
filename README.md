e8572e56f4e94b7c68afcaf102fc9263b92ec421
Nayanp760 <77923740+Nayanp760@users.noreply.github.com>
2022-01-07 00:15:19 +0530

    Update and rename README.md to kirana.py

--- README.md
+++
@@ -1,2 +0,0 @@
-# Kirana_Calculator
-It is a mini project about calculator that is coustom built for local shop with Billing feature
---
+++ kirana.py
@@ -0,0 +1,30 @@
+sum = 0
+item_price = []
+item_name = []
+Bill=[]
+while True:
+	userInput = input("Enter the item price or press q to quit: \n")
+	if userInput != 'q':
+		item_price.append(userInput)
+	else:
+		break
+	if userInput != 'q':
+		sum = sum + int(userInput)
+		print(f"Order total so far: {sum}")
+	else:
+		print(f"Your Bill total is {sum}. Thanks for shopping with us")
+		break
+
+print(f"price per object is {item_price}")
+
+while True:
+	ValueInput = input("Enter the items in order of price and press q to end : ")
+	if ValueInput!= 'q':
+		item_name.append(ValueInput)
+	else:
+		for (item1, item2) in zip(item_price, item_name):
+			Bill.append(item2 + ' = RS ' + item1)
+
+		print(f"Your Bill is : {Bill}")
+		break
+
